6a7e9568509c8d2f1ca9d75f3b282c0a391398bb
Hagen Echzell <hagen.echzell@gmail.com>
2020-07-24 19:48:25 +0200

    Fix icon transparency

--- img/icons/regenerate_icons.sh
+++ img/icons/regenerate_icons.sh
@@ -1,5 +1,6 @@
-convert icon.svg -resize 16x16 icon16x16.png
-convert icon.svg -resize 48x48 icon48x48.png
-convert icon.svg -resize 60x60 icon60x60.png
-convert icon.svg -resize 128x128 icon128x128.png
-convert icon.svg -resize 192x192 icon 192x192.png
+rm *.png
+convert icon.svg -transparent white -resize 16x16 icon16x16.png
+convert icon.svg -transparent white -resize 48x48 icon48x48.png
+convert icon.svg -transparent white -resize 60x60 icon60x60.png
+convert icon.svg -transparent white -resize 128x128 icon128x128.png
+convert icon.svg -transparent white -resize 192x192 icon192x192.png
